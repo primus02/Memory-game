cb3fe40595fe476aeeb354e45b64a8e5c383992e
AdrianaSchmit <schmit.adriana@gmail.com>
2021-02-18 20:22:55 +0000

    cards positioning fixed

--- cards.js
+++ cards.js
@@ -5,8 +5,9 @@
          for (var i = 0; i < 12; i++) {
              var card = document.querySelector("#card" + i);
              
-             card.style.left = (i === 0 ||  i === 6) ? 5 + "px" : i % 6 * 165 + 5 + "px";
+             card.style.left = (i === 0 ||  i === 6) ? 10 + "px" : i % 6 * 180 + 10 + "px";
              card.style.top = i < 6 ? 5 + "px" : 250 + "px";
+             console.log(card.style.left)
         
          }
     }
