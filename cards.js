21f5814c22123a2c47e4db1bab4e20839a366474
AdrianaSchmit <schmit.adriana@gmail.com>
2021-02-18 21:40:58 +0000

    rotation added to the cards

--- cards.js
+++ cards.js
@@ -5,10 +5,21 @@
          for (var i = 0; i < 12; i++) {
              var card = document.querySelector("#card" + i);
              
-             card.style.left = (i === 0 ||  i === 6) ? 10 + "px" : i % 6 * 180 + 10 + "px";
+             card.style.left = (i === 0 ||  i === 6) ? 15 + "px" : i % 6 * 180 + 10 + "px";
              card.style.top = i < 6 ? 5 + "px" : 250 + "px";
-             console.log(card.style.left)
+          
+
+             card.addEventListener("click", turnCard,false);
         
          }
     }
+function turnCard(){
+    var faces = this.getElementsByClassName("face");
+    faces[0].classList.toggle("turned");
+    faces[1].classList.toggle("turned");
+
+     }
+
 }());
+
+
