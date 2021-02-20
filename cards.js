67f1defb51b2f3b433d1e8c69bb7599a3a84c841
AdrianaSchmit <schmit.adriana@gmail.com>
2021-02-20 17:37:43 +0000

    front cards images positioned

--- cards.js
+++ cards.js
@@ -1,18 +1,36 @@
 (function (){
+	var images = [];
+	
+	//structure for the images
+	for(var i = 0; i < 24; i++){
+		
+		var img = {
+            src: "assets/images/" + i + ".jpg",
+			id: i%8
+		};
+		
+		//push what I created in the array
+		images.push(img);
+	}
+	
     startGame();
 
     function startGame(){
-         for (var i = 0; i < 12; i++) {
-             var card = document.querySelector("#card" + i);
-             
-             card.style.left = (i === 0 ||  i === 6) ? 15 + "px" : i % 6 * 180 + 10 + "px";
-             card.style.top = i < 6 ? 5 + "px" : 250 + "px";
-          
 
-             card.addEventListener("click", turnCard,false);
+     var frontFaces = document.getElementsByClassName("front");
         
-         }
-    }
+        for(var i = 0; i < 24; i++){
+			var card = document.querySelector("#card" + i);
+			card.style.left = (i % 8 === 0) ? 20 + "px" : (i % 8) * 130 + 10 + "px";
+            card.style.top = i < 8 ? 5 + "px" : (i < 16 ? 160 + "px" : 320 + "px");
+
+            card.addEventListener("click",turnCard,false);
+            
+            frontFaces[i].style.background = "url('"+ images[i].src +"')";
+			frontFaces[i].setAttribute("id",images[i].id);
+            
+		}
+	}
 function turnCard(){
     var faces = this.getElementsByClassName("face");
     faces[0].classList.toggle("turned");
@@ -21,5 +39,3 @@ function turnCard(){
      }
 
 }());
-
-
